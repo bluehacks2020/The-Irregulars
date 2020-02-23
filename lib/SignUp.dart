a07aabf89ed1752a882727bfa6264a3c0aa819e8
Van Philip Panugan <vpp5010207@gmail.com>
2020-02-23 11:13:07 +0800

    Final

---
+++ lib/SignUp.dart
@@ -0,0 +1,192 @@
+import 'package:flutter/material.dart';
+
+class SignUp extends StatefulWidget {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  _SignUpState createState() => new _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  final _formKey = GlobalKey<FormState>();
+  final _fnameController = TextEditingController();
+  final _mnameController = TextEditingController();
+  final _lnameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _password2Controller = TextEditingController();
+  bool isEmpty1 = true;
+  bool isEmpty2 = true;
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Sign Up"),
+      ),
+      body: Center(
+        child: ListView(
+          children: <Widget>[
+            Form(
+              key: _formKey,
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          top: 15.0, left: 25.0, right: 25.0),
+                      child: Center(
+                        child: TextFormField(
+                          controller: _fnameController,
+                          decoration: const InputDecoration(
+                            hintText: 'Enter your First Name',
+                          ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter your First Name';
+                            } else
+                              return null;
+                          },
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+                      child: Center(
+                        child: TextFormField(
+                          controller: _mnameController,
+                          decoration: const InputDecoration(
+                            hintText: 'Enter your Middle Name',
+                          ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter your Middle Name';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+                      child: Center(
+                        child: TextFormField(
+                          controller: _lnameController,
+                          decoration: const InputDecoration(
+                            hintText: 'Enter your Last Name',
+                          ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter your Last Name';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+                      child: Center(
+                        child: TextFormField(
+                          controller: _emailController,
+                          decoration: const InputDecoration(
+                            hintText: 'Enter your email',
+                          ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter your email';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+                      child: Center(
+                        child: TextFormField(
+                          controller: _passwordController,
+                          decoration: const InputDecoration(
+                            hintText: 'Enter your password',
+                          ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter your password';
+                            }
+
+                            return null;
+                          },
+                          obscureText: true,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+                      child: Center(
+                        child: TextFormField(
+                          controller: _password2Controller,
+                          decoration: const InputDecoration(
+                            hintText: 'Confirm your password',
+                          ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please confirm your password';
+                            }
+                            else if (value != _passwordController.text) {
+                              return 'Passwords do not match';
+                            }
+                            return null;
+                          },
+                          obscureText: true,
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 16.0),
+                        child: RaisedButton(
+                          onPressed: () {
+                            // Validate will return true if the form is valid, or false if
+                            // the form is invalid.
+                            if (_formKey.currentState.validate()) {
+                              //                              // Process data.
+                            }
+
+
+                            if (passValidate()==true){
+                              String _fname = _fnameController.toString();
+                              String _lname = _lnameController.toString();
+                              String _mname = _mnameController.toString();
+                              String _email = _emailController.toString();
+                              String _password = _passwordController.toString();
+
+                            } else{
+                              print("passwords do not match");
+                            }
+
+                          },
+                          child: Text('Submit'),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  bool passValidate() {
+    String password = _passwordController.text;
+    String password2 = _password2Controller.text;
+    if (password == password2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
---
+++ lib/home.page.dart
@@ -0,0 +1,76 @@
+import 'cards_section_alignment.dart';
+import 'cards_section_draggable.dart';
+import 'main.dart';
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatelessWidget {
+  bool showAlignmentCards = false;
+
+  @override
+  Widget build(BuildContext context) => new Scaffold(
+    appBar: new AppBar(
+      title: new Text('What\'s your ulam?'),
+      backgroundColor: Colors.red,
+      actions: <Widget>[
+        FlatButton.icon(
+          icon: Icon(Icons.person),
+          label: Text('Signout'),
+          onPressed: (){
+            appAuth.logout().then(
+                    (_) => Navigator.of(context).pushReplacementNamed('/login')
+            );
+          },
+        )
+      ],
+    ),
+    body:
+    Column(
+      children: <Widget>[
+        showAlignmentCards
+            ? CardsSectionAlignment(context)
+            : CardsSectionDraggable(),
+        buttonsRow()
+      ],
+    ),
+
+
+  );
+
+  Widget buttonsRow() {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 48.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          FloatingActionButton(
+            mini: true,
+            onPressed: () {},
+            backgroundColor: Colors.white,
+            child: Icon(Icons.loop, color: Colors.yellow),
+          ),
+          Padding(padding: EdgeInsets.only(right: 8.0)),
+          FloatingActionButton(
+            onPressed: () {},
+            backgroundColor: Colors.white,
+            child: Icon(Icons.close, color: Colors.red),
+          ),
+          Padding(padding: EdgeInsets.only(right: 8.0)),
+          FloatingActionButton(
+            onPressed: () {},
+            backgroundColor: Colors.white,
+            child: Icon(Icons.favorite, color: Colors.green),
+          ),
+          Padding(padding: EdgeInsets.only(right: 8.0)),
+          FloatingActionButton(
+            mini: true,
+            onPressed: () {},
+            backgroundColor: Colors.white,
+            child: Icon(Icons.star, color: Colors.blue),
+          ),
+        ],
+      ),
+    );
+  }
+}
